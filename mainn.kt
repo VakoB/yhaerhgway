69cbc21e869630ac968e52af91714c1bdfdb54ba
VakoB <115661246+VakoB@users.noreply.github.com>
2022-10-14 10:00:59 +0400

    Add files via upload

---
+++ mainn.kt
@@ -0,0 +1,13 @@
+fun main() {
+
+    var f1 = Fraction()
+    f1.numerator = 1
+    f1.denominator = 3
+}
+
+class Faction {
+    var numerator: int = 0
+    var denominator: int = 0
+    f2.numerator = 1
+    f2.denominator = 3
+}
